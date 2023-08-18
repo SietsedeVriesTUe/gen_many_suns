2d7598e0b2e5899fc6b181d86a12c1acc3edd71f
SietsedeVriesTUe <139552041+SietsedeVriesTUe@users.noreply.github.com>
2023-08-18 14:45:28 +0200

    Update gen_many_suns.py

--- gen_many_suns/gen_many_suns.py
+++ gen_many_suns/gen_many_suns.py
@@ -7,7 +7,8 @@ Make sure Radiance is installed and the script is in the same folder as the temp
 To run the code supply the name of the original (single) sun description and 
 the dimension (in pixels) of the side of a square on which the sun should be based.
 A solar angular opening of 0.533 degrees is assumed.
-authors: Signify & Sietse de Vries (sietse@sietse-de-vries.nl)
+
+This program was developed by Signify and the Eindhoven University of Technology. Author: Sietse de Vries (sietse@sietse-de-vries.nl)
 """
 
 import subprocess
