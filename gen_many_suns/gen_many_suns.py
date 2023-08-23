f8ae72cdff13a49fd993ca6138d18e02e0601930
SietsedeVriesTUe <139552041+SietsedeVriesTUe@users.noreply.github.com>
2023-08-23 16:29:58 +0200

    Update gen_many_suns.py

--- gen_many_suns/gen_many_suns.py
+++ gen_many_suns/gen_many_suns.py
@@ -8,7 +8,9 @@ To run the code supply the name of the original (single) sun description and
 the dimension (in pixels) of the side of a square on which the sun should be based.
 A solar angular opening of 0.533 degrees is assumed.
 
-This program was developed by Signify and the Eindhoven University of Technology. Author: Sietse de Vries.
+This program is developed by Sietse de Vries for Signify, during his work at the Eindhoven University of Technology. 
+The program is licensed under the BSD-3 license, see the LICENSE file.
+
 """
 
 import subprocess
